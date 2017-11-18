07f0911995125143a6f0a569e83cb56f50b3f906
gimijuny <gimijuny@gmail.com>
2017-11-18 19:48:42 +0900

    RPSQLearing commit

--- RPS/5-q-learning/test.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-
-choices = ['gawi', 'bawi', 'bo']
-
-print("test")
-
-def show_me_the_hand():
-    print("def")
-    return choice(choices[:2])
---
+++ RPSQLearning/RPSPlay.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+from RPSTrain import RPSEnvironment, X, W1, b1, input_layer, W2, b2, hidden_layer, W3, b3, output_layer, Y, cost, optimizer
+import tensorflow as tf
+import os
+
+#------------------------------------------------------------
+# 변수 설정
+#------------------------------------------------------------
+RPS_PLAYER1 = 1
+RPS_PLAYER2 = 2
+rules = {0: 1, 1: 2, 2: 0}
+
+#------------------------------------------------------------
+
+#------------------------------------------------------------
+# 게임 플레이 함수
+#------------------------------------------------------------
+def playGame(env, sess):
+
+	env.reset()
+
+	gameOver = False
+	currentPlayer = RPS_PLAYER1
+
+	player_score = 0
+	ai_score = 0
+
+	while gameOver != True:
+		action = - 9999
+		
+		if currentPlayer == RPS_PLAYER1:
+			currentState = env.getState()
+		else:
+			currentState = env.getStateInverse()
+
+		action = env.getAction(sess, currentState)
+		nextState, reward, gameOver = env.act(currentPlayer, action)
+
+		player = input("Enter your choice (R/P/S): ")
+		player = player.upper()
+		while player != "R" and player != "P" and player != "S":
+			player = input("Enter your choice (R/P/S): ")
+			player = player.upper()
+
+		if player == "R":
+			RSP = 1
+		elif player == "P":
+			RSP = 2
+		else:
+			RSP = 0
+
+		if rules[RSP] == action:
+			player_score += 1
+			print("Player Win")
+			print(player_score, " : ", ai_score)
+		elif rules[action] == RSP:
+			ai_score += 1
+			print("AI Win")
+			print(player_score, " : ", ai_score)
+		else:
+			print("It's tie")
+
+		if player_score >= 3:
+			gameOver = True
+			print("Game Over!! Player Winner")
+		elif ai_score >= 3:
+			gameOver = True
+			print("Game Over!! Player Winner")
+
+#------------------------------------------------------------
+
+#------------------------------------------------------------
+# 메인 함수
+#------------------------------------------------------------
+def main(_):
+
+	# 환경 인스턴스 생성
+	env = RPSEnvironment()
+
+	# 텐서플로우 초기화
+	sess = tf.Session()
+	sess.run(tf.global_variables_initializer())
+
+	# 세이브 설정
+	saver = tf.train.Saver()
+
+	# 모델 로드
+	if os.path.isfile(os.getcwd() + "/RPSModel.ckpt.index") == True:
+		saver.restore(sess, os.getcwd() + "/RPSModel.ckpt")
+		print('saved model is loaded!')
+	
+	# 게임 플레이
+	playGame(env, sess)
+	
+	# 세션 종료
+	sess.close()
+#------------------------------------------------------------
+
+#------------------------------------------------------------
+# 메인 함수 실행
+#------------------------------------------------------------
+if __name__ == '__main__':
+	tf.app.run()
+#------------------------------------------------------------
+
