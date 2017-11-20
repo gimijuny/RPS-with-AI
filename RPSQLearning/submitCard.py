0ce08e50b18720be6f241e337821cb6a032af88f
gimijuny <gimijuny@gmail.com>
2017-11-20 12:37:00 +0900

    텐서플로우 테스트 코드 추가

--- RPSQLearning/submitCard.py
+++ RPSQLearning/submitCard.py
@@ -18,6 +18,15 @@ score = sys.argv[3]
 env = RPSEnvironment()
 env.reset()
 
+# 텐서플로우 테스트
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a+b))
+
 # 텐서플로우 초기화
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
--- RPSQLearning/submitMatch.py
+++ RPSQLearning/submitMatch.py
@@ -1,6 +1,5 @@
 from RPSTrain import RPSEnvironment, X, W1, b1, input_layer, W2, b2, hidden_layer, W3, b3, output_layer, Y, cost, optimizer
 import tensorflow as tf
-import random
 import sys
 
 #------------------------------------------------------------
@@ -9,6 +8,23 @@ import sys
 RPS_PLAYER1 = 1
 RPS_PLAYER2 = 2
 rules = {0: 1, 1: 2, 2: 0}
+
+# 환경 인스턴스 생성
+env = RPSEnvironment()
+env.reset()
+
+# 텐서플로우 테스트
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a+b))
+
+# 텐서플로우 초기화
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
 playerId = sys.argv[1]
 match = sys.argv[2]
